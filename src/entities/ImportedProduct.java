fff29d1358227bcd6e7b283b1fe4aa4271400257
VinoLemos <vlemosdeoliveira@gmail.com>
2021-09-05 18:16:59 -0300

    Criada classe ImportedProduct, subclasse da classe Product

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class ImportedProduct extends Product{
+	
+	private Double customsFee;
+	public ImportedProduct() {
+		
+	}
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
+	private Double totalPrice() {
+		return super.getPrice()+customsFee;
+	}
+	
+}
