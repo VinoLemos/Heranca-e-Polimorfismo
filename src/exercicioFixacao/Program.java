9c856cb331195ebfe6f5a3fc12546cd81721b165
VinoLemos <vlemosdeoliveira@gmail.com>
2021-09-06 17:57:57 -0300

    Criado switch case que verifica se o produto e comum, usado ou importado

--- src/exercicioFixacao/Program.java
+++ src/exercicioFixacao/Program.java
@@ -1,20 +1,25 @@
 package exercicioFixacao;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.ImportedProduct;
 import entities.Product;
+import entities.UsedProduct;
 
 public class Program {
 /*
  * Este programa lerá N produtos fornecidos pelo usuário, e ao final mostrará a etiqueta de preço de cada
  * produto na ordem em que foram digitados.
  */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 		List<Product> products = new ArrayList<>();
 		
 		
@@ -33,8 +38,28 @@ public class Program {
 			
 			System.out.print("Price: ");
 			double price = sc.nextDouble();
-			
-			products.add(new Product(name, price));
+// Case switch que verifica se o produto é comum, usado ou importado			
+			switch (type) {
+			case 'c':
+		//Retorna nome e preço caso seja comum
+				products.add(new Product(name, price));
+				break;
+				
+			case 'u':
+		//Retorna nome, preço e data de fabricação caso seja usado
+				System.out.print("Manufacture date: ");
+				Date manufactureDate = sdf.parse(sc.next());
+				
+				products.add(new UsedProduct(name, price, manufactureDate));
+				break;
+			case 'i':
+		//Retorna nome, preço e taxa de importação caso seja importado
+				System.out.print("Customs fee: ");
+				double customsFee = sc.nextDouble();
+				
+				products.add(new ImportedProduct(name, price, customsFee));
+				break;
+			}
 			
 		}
 		
