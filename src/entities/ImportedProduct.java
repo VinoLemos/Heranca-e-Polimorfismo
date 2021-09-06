9ba9a62c1251207280b0e4ba6ee8101fa2add5d0
VinoLemos <vlemosdeoliveira@gmail.com>
2021-09-06 18:17:26 -0300

    Corrigido pequeno erro de semantica na funcao priceTag()

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -6,26 +6,29 @@ public class ImportedProduct extends Product{
 	public ImportedProduct() {
 		
 	}
+
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+	
 	public Double getCustomsFee() {
 		return customsFee;
 	}
 	public void setCustomsFee(Double customsFee) {
 		this.customsFee = customsFee;
 	}
-	public ImportedProduct(String name, Double price, Double customsFee) {
-		super(name, price);
-		this.customsFee = customsFee;
-	}
 	
 	private Double totalPrice() {
-		return super.getPrice()+customsFee;
+		return getPrice()+customsFee;
 	}
-	@Override
+
 	public String priceTag() {
-		return super.getName()
-				+ " $"
-				+ String.format("%.2f", (totalPrice()))
-				+ "Customs fee: "
-				+ String.format("%.2", customsFee);
+		return getName()
+				+ " $ "
+				+ String.format("%.2f", totalPrice())
+				+ " (Customs fee: $ "
+				+ String.format("%.2f", customsFee)
+				+")";
 	}
 }
